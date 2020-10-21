72478c27b736ec3dc65c127d67f4f8d782d288fd
Tropical Shadow <ddlogger1233@gmail.com>
2020-10-21 15:06:43 +0100

    INIT

---
+++ README.md
@@ -0,0 +1,1 @@
+### TeleMatrix Sheet
---
+++ bot/main.py
@@ -0,0 +1,31 @@
+from discord.ext.commands import Bot
+from discord import __version__
+from os import listdir,getenv
+
+TOKEN = getenv("DISCORD_TOKEN")
+
+
+description = """A Fully Custom Discord Bot"""
+command_prefix = ("--",)
+
+bot = Bot(command_prefix=command_prefix,description=description)
+
+print ("Discord Version:",__version__)
+print ("Loading Bot, Please Wait")
+
+@bot.command(pass_context=True)
+async def ping(ctx,*args,**kwargs):
+    await ctx.send(content='Pinging')
+
+cogs = sorted(listdir("./cogs"))
+for cog in cogs:
+      if cog.endswith(".py"):
+         try:
+            cog = f"cogs.{cog.replace('.py','')}"
+            bot.load_extension(cog)
+            print(f"loaded {cog}")
+         except Exception as e:
+            print(f"{cog} can not be loaded")
+            print(e)
+
+bot.run(TOKEN,bot=True,reconnect=True)
