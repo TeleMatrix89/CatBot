127e23eadb9153356431bc5d8a994d166276636d
TeleMatrix89 <bewitchedcat@gmail.com>
2020-11-04 21:11:04 +0000

    tried fixing error

--- bot/cogs/FunCommands.py
+++ bot/cogs/FunCommands.py
@@ -20,7 +20,7 @@ class FunCommands(Cog):
     @commands.command(name="test")
     async def ching_command(self,ctx,*args,**kwargs):
         img = File('dog.png')
-        await ctx.send(img)
+        await ctx.send(file=img)
     
 
     
