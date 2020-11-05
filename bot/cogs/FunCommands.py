c0d51a1747eb5120fc96fc0c9dbb94b6829a97b8
TeleMatrix89 <bewitchedcat@gmail.com>
2020-11-05 12:25:29 +0000

    added commands, commented out <>test

--- bot/cogs/FunCommands.py
+++ bot/cogs/FunCommands.py
@@ -14,15 +14,29 @@ class FunCommands(Cog):
     async def hing_command(self,ctx,*args,**kwargs):
         if ctx.author.id == 268122068007124993:
             await ctx.send("Hello ree master, my creator")
+        elif ctx.author.id == 759060138006020146:
+            await ctx.send("FUS RO REE")
+        elif ctx.author.id == 541615594202464257:
+            await ctx.send("Joke's on you, I'm into ree... hehehe")
+        elif ctx.author.id == 303480386271313921:
+            await ctx.send("Ree, y toes are big cold.... poop")
+    
         else:
             await ctx.send("Hello, you have been blessed by the ree.")
 
+    @commands.command(name="soul")
+    async def ching_command(self,ctx,*args,**kwargs):
+        if ctx.author.id == 303480386271313921:
+            await ctx.send("My fave sister)
+        else:
+            await ctx.send("You're not Soul... go away")
+'''
     @commands.command(name="test")
     async def ing_command(self,ctx,*args,**kwargs):
         #fil = File('img.png',filename="img.png")
         #await ctx.send(content="ping",file=fil)
         fil = File('img.png',filename="img.png")
         await ctx.send(content="ping",file=fil)
-
+'''
 def setup(bot):
     bot.add_cog(FunCommands(bot))
