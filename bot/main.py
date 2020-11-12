7c735b0cb5fd619a5de0fcbe264f80de17738ddb
TeleMatrix89 <bewitchedcat@gmail.com>
2020-11-12 21:57:52 +0000

    error

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,6 @@
 from discord.ext.commands import Bot
 from discord.errors import LoginFailure
-from discord import __version__, Intent
+from discord import __version__, Intents
 from os import listdir,getenv,chdir
 from os.path import dirname,abspath,join
 
